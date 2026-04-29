d93f580704ac5a093fa1bac53cf76da3870529cd
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-29 21:41:19 +0900

    MemberDto.java: API 통신에 사용되는 회원 데이터 전송 객체.

--- worksight-api/src/main/java/com/worksight/api/dto/MemberDto.java
+++ worksight-api/src/main/java/com/worksight/api/dto/MemberDto.java
@@ -1,4 +1,9 @@
 package com.worksight.api.dto;
 
+import com.worksight.api.entity.Role;
+
 public class MemberDto {
+    public record SignUpRequest(String username, String password, Role role) {}
+    public record LoginRequest(String username, String password) {}
+    public record MemberResponse(Long id, String username, Role role, Long balance) {}
 }
