6ae8113b891665ab80e423b09587772a78456630
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-29 21:41:59 +0900

    MemberController.java: 회원 관련 REST API 엔드포인트 구현.

--- worksight-api/src/main/java/com/worksight/api/controller/MemberController.java
+++ worksight-api/src/main/java/com/worksight/api/controller/MemberController.java
@@ -1,4 +1,26 @@
 package com.worksight.api.controller;
 
+import com.worksight.api.dto.MemberDto.*;
+import com.worksight.api.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponse> signUp(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(memberService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MemberResponse> login(@RequestBody LoginRequest request) {
+        // 실제 구현에서는 JWT 토큰 반환
+        return ResponseEntity.ok(memberService.login(request));
+    }
 }
