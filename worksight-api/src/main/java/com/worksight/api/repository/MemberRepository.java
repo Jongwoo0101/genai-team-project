cbaefd2aa713ff7c0663496fa56e6ad3b1c808d9
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2026-04-29 21:41:34 +0900

    MemberRepository.java: DB 접근을 위한 Spring Data JPA 인터페이스.

--- worksight-api/src/main/java/com/worksight/api/repository/MemberRepository.java
+++ worksight-api/src/main/java/com/worksight/api/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.worksight.api.repository;
 
-public class MemberRepository {
+import com.worksight.api.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
